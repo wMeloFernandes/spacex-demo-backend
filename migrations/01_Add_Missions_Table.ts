122456d4449ed615ec98fea4330e1e6069351062
Willian Melo <w.melo.fernandes.un@gmail.com>
2024-05-31 03:47:12 -0300

    add migration for missions table

---
+++ migrations/01_Add_Missions_Table.ts
@@ -0,0 +1,36 @@
+module.exports = {
+    up(query, DataTypes) {
+        return query.createTable('missions', {
+            id: {
+                type: DataTypes.UUID,
+                allowNull: false,
+                primaryKey: true,
+                defaultValue: DataTypes.UUIDV4
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            shipId: {
+                type: DataTypes.UUID,
+                allowNull: false,
+                references: {
+                    model: 'ships',
+                    key: 'id'
+                }
+            },
+            createdAt: {
+                type: DataTypes.DATE,
+                allowNull: false
+            },
+            updatedAt: {
+                type: DataTypes.DATE,
+                allowNull: false
+            },
+        })
+    },
+
+    down(query) {
+        return query.dropTable('missions')
+    },
+};
