1405dbed9e4a9f6840c22622d55888d1743dbbc2
Willian Melo <w.melo.fernandes.un@gmail.com>
2024-05-31 03:15:50 -0300

    run migration when populate script is run

--- helpers/populate.ts
+++ helpers/populate.ts
@@ -5,9 +5,12 @@ if (process.env.NODE_ENV !== 'production') {
 import { db } from '../models';
 import { cleanDb } from '../helpers/testHelpers';
 import fetch from 'node-fetch';
+import { umzug } from '../migrate'
 
 const populate = async () => {
   await cleanDb();
+  // Checks migrations and run them if they are not already applied.
+  await umzug.up();
   console.log('Populating database...');
 
   const ships = await fetch('https://spacex-production.up.railway.app/api/graphql', {
