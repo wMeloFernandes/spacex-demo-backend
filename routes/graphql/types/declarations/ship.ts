d092ae28629b06e57bf582ff154460a943ad5e50
Willian Melo <w.melo.fernandes.un@gmail.com>
2024-05-31 04:16:48 -0300

    add Mission type to graphql declaration

--- routes/graphql/types/declarations/ship.ts
+++ routes/graphql/types/declarations/ship.ts
@@ -1,12 +1,18 @@
 import { gql } from 'graphql-tag';
 
 export const Ship = gql`
+  type Mission {
+    id: ID!
+    name: String
+  }
+
   type Ship {
     id: ID!
     name: String
     class: String
     image: String
     active: Boolean!
+    Missions: [Mission]
   }
 
   input ShipsInput {
