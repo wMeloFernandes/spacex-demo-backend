10c341ff7f444615481af1d309528ec74ce2e623
Willian Melo <w.melo.fernandes.un@gmail.com>
2024-05-31 04:16:25 -0300

    bring mission data from relation in get query

--- controllers/ship.ts
+++ controllers/ship.ts
@@ -1,10 +1,11 @@
 import { PaginationInput } from '../common/types/backend';
 import { AuthScope } from '../config';
 import { db } from '../models';
+import { Mission } from '../models/mission';
 import { ShipAttributes } from '../models/ship';
 
 const get = async ({ pagination }: { pagination: PaginationInput }, authScope: AuthScope): Promise<ShipAttributes[]> => {
-  const ships = await db.Ship.findAll();
+  const ships = await db.Ship.findAll({ include: [{ model: Mission, required: true }] });
 
   return ships;
 };
